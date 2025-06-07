d0117ac298df2ca2dde46180cc8cacb64f4766ce
prathamesh-mathapatik <prathamesh20@navgurukul.org>
2025-06-07 17:26:30 +0530

    make context api

--- app/provider.js
+++ app/provider.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { Header } from './_componet/Header'
-import { Hero } from './_componet/Hero'
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Header } from "./_componet/Header";
+import { Hero } from "./_componet/Hero";
+import { onAuthStateChanged } from "firebase/auth";
+import { AuthContext } from "./_context/Authcontext";
 
-export const Provider = ({children}) => {
+export const Provider = ({ children }) => {
+  const [user,setUser]=useState([])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user)
+      console.log(user);
+    });
+    return () => unsubscribe();
+  }, []);
   return (
-    <div className='container m-auto'>
-        <NextThemesProvider attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange >
-            {children}
-            <Header/>
-            <Hero/>
-        </NextThemesProvider>
-        </div>
-  )
+    <div className="container m-auto">
+      <AuthContext.Provider value={user}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Header />
+        <Hero />
+      </NextThemesProvider>
+      </AuthContext.Provider>
+    </div>
+  );
+};
+
+export const useAuthContex=()=>{
+  const context=useContext(AuthContext)
+  return context
 }
